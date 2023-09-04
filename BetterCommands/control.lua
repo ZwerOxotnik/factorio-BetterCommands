a9e24478b0b225fb6ad2c6c2d0f6188597e7bc0c
ZwerOxotnik <zweroxotnik@gmail.com>
2023-09-04 11:46:17 +0300

    Improve

--- BetterCommands/control.lua
+++ BetterCommands/control.lua
@@ -1,6 +1,3 @@
--- TODO: fix localization, handle settings on events more accurately using global data
-
-
 ---@class BetterCommands : module
 ---@source https://github.com/ZwerOxotnik/factorio-BetterCommands
 ---@module "BetterCommands.control"
@@ -26,7 +23,11 @@ local M = {
 ---@field max_input_length uint # Max amount of characters for command (default: 500)
 
 
----@type table<string, BetterCommand>
+---@class _BetterCommand
+---@field name string
+
+
+---@type table<string, function>
 local _all_commands = {} -- commands from other modules
 
 
@@ -50,16 +51,6 @@ end
 ---@param s string
 local function trim(s)
 	return s:match'^%s*(.*%S)' or ''
-end
-
-
----@return boolean
-local function remove_command(command_name)
-	if commands.remove_command((M.COMMAND_PREFIX or MOD_SHORT_NAME) .. command_name) then
-		return true
-	else
-		return commands.remove_command(command_name)
-	end
 end
 
 
@@ -105,18 +96,14 @@ end
 local function disable_setting(error_message, player_index, orig_command_name)
 	print_to_caller(error_message, player_index)
 
-	local is_message_sended = false
 	if game then
 		for _, player in pairs(game.connected_players) do
 			if player.valid and player.admin then
 				player.print(error_message)
-				is_message_sended = true
 			end
 		end
 	end
-	if is_message_sended == false then
-		log(error_message)
-	end
+	log(error_message)
 
 	-- Turns off the command
 	if orig_command_name then
@@ -136,23 +123,25 @@ local _INPUT_TYPES = {
 	team   = _TEAM_INPUT_TYPE
 }
 
+
+---@param orig_command_name string
 ---@param command_settings BetterCommand
 ---@param original_func function
----@return boolean
+---@return string? # command name
 local function add_custom_command(orig_command_name, command_settings, original_func)
 	local input_type = _INPUT_TYPES[command_settings.input_type]
 	local is_allowed_empty_args = command_settings.is_allowed_empty_args
-	local command_name = command_settings.name
-	if commands.commands[command_name] then
-		command_name = (M.COMMAND_PREFIX or MOD_SHORT_NAME) .. command_name
-		if commands.commands[command_name] then
-			return false
+	local new_command_name = command_settings.name
+	if commands.commands[new_command_name] then
+		new_command_name = (M.COMMAND_PREFIX or MOD_SHORT_NAME) .. new_command_name
+		if commands.commands[new_command_name] then
+			return
 		end
 	end
 
 	local max_input_length = command_settings.max_input_length or M.DEFAULT_MAX_INPUT_LENGTH
 	local command_description = command_settings.description or {script.mod_name .. "-commands." .. command_settings.name}
-	commands.add_command(command_name, command_description, function(cmd)
+	commands.add_command(new_command_name, command_description, function(cmd)
 		if cmd.player_index == 0 then
 			if command_settings.allow_for_server == false then
 				print({"prohibited-server-command"})
@@ -169,7 +158,7 @@ local function add_custom_command(orig_command_name, command_settings, original_
 
 		if cmd.parameter == nil then
 			if is_allowed_empty_args == false then
-				print_to_caller({"", '/' .. command_name .. ' ', command_description}, cmd.player_index)
+				print_to_caller({"", '/' .. new_command_name .. ' ', command_description}, cmd.player_index)
 				return
 			end
 		elseif #cmd.parameter > Mmax_input_length then
@@ -214,7 +203,7 @@ local function add_custom_command(orig_command_name, command_settings, original_
 		end
 	end)
 
-	return true
+	return new_command_name
 end
 
 
@@ -230,84 +219,56 @@ function M:handle_custom_commands(module)
 		return false
 	end
 
-	for _, added_commands in pairs(_all_commands) do
-		if added_commands == module.commands then
-			log("Current commands was added before")
-			return true
-		end
-	end
-	table.insert(_all_commands, module.commands)
-
 	for command_name, func in pairs(module.commands) do
 		local command_settings = (SWITCHABLE_COMMANDS and SWITCHABLE_COMMANDS[command_name])
-			or (CONST_COMMANDS and CONST_COMMANDS[command_name])
-			or {}
-		command_settings.name = command_settings.name or command_name
-		local setting = nil
-		if SWITCHABLE_COMMANDS[command_name] then
-			setting = settings.global[(M.COMMAND_PREFIX or MOD_SHORT_NAME) .. command_name]
+		or (CONST_COMMANDS and CONST_COMMANDS[command_name])
+		if not command_settings then
+			goto continue
 		end
 
-		if setting == nil then
-			local is_added = add_custom_command(command_name, command_settings, func)
-			if is_added == false then
-				log(script.mod_name .. " can't add command \"" .. command_settings.name .. "\"")
-			end
-		elseif setting.value then
-			local is_added = add_custom_command(command_name, command_settings, func)
-			if is_added == false then
-				local message = script.mod_name .. " can't add command \"" .. command_settings.name .. "\""
-				disable_setting(message, nil, command_name)
-			end
+		if _all_commands[command_name] then
+			log(string.format("[ERROR] \"%s\" command was added before", command_name))
 		else
-			remove_command(command_settings.name)
+			_all_commands[command_name] = func
 		end
+
+		:: continue ::
 	end
 
 	return true
 end
 
 
----@param command_name string original command name
----@return function?
-local function find_func_by_command_name(command_name)
-	for _, some_commands in pairs(_all_commands) do
-		local func = some_commands[command_name]
-		if func then
-			return func
-		end
-	end
-end
-
-
----@return boolean
-local function on_runtime_mod_setting_changed(event)
+function M.on_runtime_mod_setting_changed(event)
 	if event.setting_type ~= "runtime-global" then return end
 	local setting_name = event.setting
-	if string.find(setting_name, '^' .. (M.COMMAND_PREFIX or MOD_SHORT_NAME)) ~= 1 then return end
+	if string.find(setting_name, '^' .. (M.COMMAND_PREFIX or MOD_SHORT_NAME)) == nil then return end
 
 	local command_name = string.gsub(setting_name, '^' .. (M.COMMAND_PREFIX or MOD_SHORT_NAME), "")
-	local func = find_func_by_command_name(command_name)
+	local func = _all_commands[command_name]
 	if func == nil then
-		log("Didn't find '" .. command_name .. "' among commands in modules")
+		log("Didn't find '" .. command_name .. "' among commands")
+		return
 	end
+
 	local command_settings = SWITCHABLE_COMMANDS[command_name] or {}
-	local state = settings.global[setting_name].value
 	command_settings.name = command_settings.name or command_name
-	if state == true then
-		local is_added = add_custom_command(command_name, command_settings, func)
-		if is_added then
-			game.print("Added command: " .. command_settings.name or command_name)
+	local activated_commands = global.BetterCommands.activated_commands
+	if settings.global[setting_name].value == true then
+		local new_command_name = add_custom_command(command_name, command_settings, func)
+		if new_command_name then
+			game.print("Added command: " .. new_command_name)
+			activated_commands[command_name] = new_command_name
 		else
 			local message = script.mod_name .. " can't add command \"" .. command_settings.name .. "\""
 			disable_setting(message, nil, command_name)
 		end
-	else
-		remove_command(command_settings.name or command_name)
-		game.print("Removed command: " .. command_settings.name or command_name)
+	elseif activated_commands[command_name] then
+		local command_name_in_game = activated_commands[command_name]
+		commands.remove_command(command_name_in_game)
+		game.print("Removed command: " .. command_name_in_game)
+		activated_commands[command_name] = nil
 	end
-
-	return true
 end
 
 
@@ -340,8 +301,89 @@ function M.create_settings(mod_name, mod_short_name)
 end
 
 
+local _is_commands_added = false
+function M.add_commands()
+	if _is_commands_added then return end
+
+	local activated_commands = global.BetterCommands.activated_commands
+	if game then
+		for command_name, original_func in pairs(_all_commands) do
+			local command_settings = (SWITCHABLE_COMMANDS and SWITCHABLE_COMMANDS[command_name])
+				or (CONST_COMMANDS and CONST_COMMANDS[command_name])
+			if not command_settings then
+				goto continue
+			end
+
+			command_settings.name = command_settings.name or command_name
+			local setting = nil
+			if SWITCHABLE_COMMANDS[command_name] then
+				setting = settings.global[(M.COMMAND_PREFIX or MOD_SHORT_NAME) .. command_name]
+			end
+
+			if setting == nil then
+				local new_command_name = add_custom_command(command_name, command_settings, original_func)
+				if new_command_name then
+					activated_commands[command_name] = new_command_name
+				else
+					log(script.mod_name .. " can't add command \"" .. command_settings.name .. "\"")
+				end
+			elseif setting.value then
+				local new_command_name = add_custom_command(command_name, command_settings, original_func)
+				if new_command_name then
+					activated_commands[command_name] = new_command_name
+				else
+					local message = script.mod_name .. " can't add command \"" .. command_settings.name .. "\""
+					disable_setting(message, nil, command_name)
+				end
+			elseif activated_commands[command_name] then
+				commands.remove_command(activated_commands[command_name])
+				activated_commands[command_name] = nil
+			end
+
+			::continue::
+		end
+	else
+		for command_name, command_name_in_game in ipairs(activated_commands) do
+			local command_settings = (SWITCHABLE_COMMANDS and SWITCHABLE_COMMANDS[command_name])
+				or (CONST_COMMANDS and CONST_COMMANDS[command_name])
+			if not command_settings then
+				goto continue
+			end
+			local func = _all_commands[command_name]
+			if not func then
+				goto continue
+			end
+
+			add_custom_command(command_name_in_game, command_settings, func)
+
+		    ::continue::
+		end
+	end
+
+	_is_commands_added = true
+end
+
+
+function M.on_init()
+	global.BetterCommands = global.BetterCommands or {}
+	---@type table<string, string>
+	global.BetterCommands.activated_commands = global.BetterCommands.activated_commands or {}
+
+	M.add_commands()
+end
+
+function M.on_load()
+	M.add_commands()
+end
+
+
+function M.on_configuration_changed()
+	M.add_commands()
+end
+
+
 M.events = {
-	[defines.events.on_runtime_mod_setting_changed] = on_runtime_mod_setting_changed
+	[defines.events.on_runtime_mod_setting_changed] = M.on_runtime_mod_setting_changed
 }
 
 
